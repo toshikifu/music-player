3cda9ecc2c0f04d59c05e2c9916ece2258015430
Toshiki <toshiki.fujii@outlook.com>
2024-11-24 23:51:58 +0900

    fix: Fix useState initialization in useNavigationSearch hook

--- src/hooks/useNavigationSearch.tsx
+++ src/hooks/useNavigationSearch.tsx
@@ -13,7 +13,7 @@ export const useNavigationSearch = ({
 }: {
 	searchBarOptions?: SearchBarProps
 }) => {
-	const [search, setSearch] = useState<string>('')
+	const [search, setSearch] = useState('')
 
 	const navigation = useNavigation()
 
@@ -23,7 +23,7 @@ export const useNavigationSearch = ({
 
 	useLayoutEffect(() => {
 		navigation.setOptions({
-			headerSearchOptions: {
+			headerSearchBarOptions: {
 				...defaultSearchOptions,
 				...searchBarOptions,
 				onChangeText: handleOnChangeText,
