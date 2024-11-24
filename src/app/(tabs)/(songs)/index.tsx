28a4e5eddc70ff770adc81d9d6a1cd114745f9fa
Toshiki <toshiki.fujii@outlook.com>
2024-11-24 23:40:10 +0900

    feat: Improve scroll behavior in SongsScreen

--- src/app/(tabs)/(songs)/index.tsx
+++ src/app/(tabs)/(songs)/index.tsx
@@ -1,11 +1,15 @@
 import { TracksList } from '@/components/TracksList'
+import { screenPadding } from '@/constants/tokens'
 import { defaultStyles } from '@/styles'
 import { ScrollView, Text, View } from 'react-native'
 
 const SongsScreen = () => {
 	return (
 		<View style={defaultStyles.container}>
-			<ScrollView>
+			<ScrollView
+				contentInsetAdjustmentBehavior="automatic"
+				style={{ paddingHorizontal: screenPadding.horizontal }}
+			>
 				<TracksList scrollEnabled={false} />
 			</ScrollView>
 		</View>
