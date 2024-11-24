c1c05d2283ebf82f6a522811f8f59d02648eb5b5
Toshiki <toshiki.fujii@outlook.com>
2024-11-25 00:00:25 +0900

    feat: implement song filter

--- src/app/(tabs)/(songs)/index.tsx
+++ src/app/(tabs)/(songs)/index.tsx
@@ -3,6 +3,9 @@ import { screenPadding } from '@/constants/tokens'
 import { useNavigationSearch } from '@/hooks/useNavigationSearch'
 import { defaultStyles } from '@/styles'
 import { ScrollView, Text, View } from 'react-native'
+import library from '@/assets/data/library.json'
+import { useMemo } from 'react'
+import { trackTitleFilter } from '@/helpers/filter'
 
 const SongsScreen = () => {
 	const search = useNavigationSearch({
@@ -10,13 +13,20 @@ const SongsScreen = () => {
 			placeholder: 'Search songs',
 		},
 	})
+
+	const filteredSongs = useMemo(() => {
+		if (!search) return library
+
+		return library.filter(trackTitleFilter(search))
+	}, [search])
+
 	return (
 		<View style={defaultStyles.container}>
 			<ScrollView
 				contentInsetAdjustmentBehavior="automatic"
 				style={{ paddingHorizontal: screenPadding.horizontal }}
 			>
-				<TracksList scrollEnabled={false} />
+				<TracksList scrollEnabled={false} tracks={filteredSongs} />
 			</ScrollView>
 		</View>
 	)
